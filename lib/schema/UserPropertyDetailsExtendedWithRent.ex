3a4c68e21710c37dea97cf5da1770baf666d66a1
dssecret <webmaster@deek.sh>
2025-07-30 21:54:46 +0000

    [torngen] Generated v3.0.2

---
+++ lib/schema/UserPropertyDetailsExtendedWithRent.ex
@@ -0,0 +1,45 @@
+defmodule Torngen.Client.Schema.UserPropertyDetailsExtendedWithRent do
+  @moduledoc false
+
+  @behaviour Torngen.Client.Schema
+
+  defstruct [:values]
+
+  @type t :: %__MODULE__{
+          values: [
+            %{
+              :used_by => [Torngen.Client.Schema.BasicUser.t()],
+              :status => String.t(),
+              :rented_by => Torngen.Client.Schema.BasicUser.t()
+            }
+            | Torngen.Client.Schema.UserPropertyBasicDetails.t()
+          ]
+        }
+
+  @impl true
+  def parse(%{} = data) do
+    %__MODULE__{
+      values: [
+        data
+        |> Torngen.Client.Schema.parse(
+          {:object,
+           %{
+             "rented_by" => Torngen.Client.Schema.BasicUser,
+             "status" => {:enum, :string, ["none", "in_use"]},
+             "used_by" => {:array, Torngen.Client.Schema.BasicUser}
+           }}
+        ),
+        data |> Torngen.Client.Schema.parse(Torngen.Client.Schema.UserPropertyBasicDetails)
+      ]
+    }
+  end
+
+  @impl true
+  def parse(_data), do: nil
+
+  @impl true
+  def validate?(%{} = _data), do: true
+
+  @impl true
+  def validate?(_data), do: false
+end
