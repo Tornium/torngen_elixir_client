39fdfec22a96fb426fe9ba646cfc2aa8560f492e
dssecret <webmaster@deek.sh>
2025-12-01 17:25:48 +0000

    [torngen] Generated v4.7.0

---
+++ lib/schema/UserEquipmentResponse.ex
@@ -0,0 +1,55 @@
+defmodule Torngen.Client.Schema.UserEquipmentResponse do
+  @moduledoc false
+
+  use Torngen.Client.SchemaObjectAccess, deprecated: []
+
+  @behaviour Torngen.Client.Schema
+
+  @keys [:equipment, :clothing]
+
+  defstruct [
+    :equipment,
+    :clothing
+  ]
+
+  @type t :: %__MODULE__{
+          equipment: [Torngen.Client.Schema.UserEquipment.t()],
+          clothing: [Torngen.Client.Schema.UserClothing.t()]
+        }
+
+  @impl true
+  def parse(%{} = data) do
+    %__MODULE__{
+      equipment:
+        data
+        |> Map.get("equipment")
+        |> Torngen.Client.Schema.parse({:array, Torngen.Client.Schema.UserEquipment}),
+      clothing:
+        data
+        |> Map.get("clothing")
+        |> Torngen.Client.Schema.parse({:array, Torngen.Client.Schema.UserClothing})
+    }
+  end
+
+  @impl true
+  def parse(_data), do: nil
+
+  @impl true
+  def validate?(%{} = data) do
+    @keys
+    |> Enum.map(fn key -> {key, Map.get(data, Atom.to_string(key))} end)
+    |> Enum.map(fn {key, value} -> validate_key?(key, value) end)
+    |> Enum.all?()
+  end
+
+  defp validate_key?(:equipment, value) do
+    Torngen.Client.Schema.validate?(value, {:array, Torngen.Client.Schema.UserEquipment})
+  end
+
+  defp validate_key?(:clothing, value) do
+    Torngen.Client.Schema.validate?(value, {:array, Torngen.Client.Schema.UserClothing})
+  end
+
+  @spec keys() :: list(atom())
+  def keys(), do: @keys
+end
