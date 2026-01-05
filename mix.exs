48a84b00ed597cccd88cc77d8ac744fc8a38168e
tiksan <webmaster@deek.sh>
2026-01-05 16:55:03 -0600

    [deps] Fixed torngen version
    Signed-off-by: tiksan <webmaster@deek.sh>

--- mix.exs
+++ mix.exs
@@ -25,7 +25,7 @@ defmodule TorngenElixirClient.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:torngen, "~> 0.1.5", runtime: false},
+      {:torngen, "~> 0.1", runtime: false},
       {:ex_doc, "~> 0.34", only: :dev, runtime: false, optional: true},
       {:req, "~> 0.5", only: :test}
     ]
